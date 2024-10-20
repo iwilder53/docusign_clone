52eaf6849d266933bab4ea68654d8cf03d68b631
Y Bagaria <yashbgdi@gmail.com>
2024-10-20 17:40:27 +0530

    auth bug

--- app/App/page.jsx
+++ app/App/page.jsx
@@ -1,26 +1,24 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import SignIn from '@/components/SignIn/SignIn';
+import SignIn from "@/components/SignIn/SignIn";
 
+import Header from "@/components/Header";
 
-import Header from '@/components/Header';
+import Welcome from "@/components/Welcome";
+import { auth, generateUserDocument } from "@/app/firebase/firebase";
+import { setUser, selectUser } from "@/app/firebase/firebaseSlice";
 
-import Welcome from '@/components/Welcome';
-import { auth, generateUserDocument } from '@/app/firebase/firebase';
-import { setUser, selectUser } from '@/app/firebase/firebaseSlice';
-
-import './app.css';
-import {  onAuthStateChanged } from "firebase/auth";
-import dynamic from 'next/dynamic';
-
-
+import "./app.css";
+import { onAuthStateChanged } from "firebase/auth";
+import dynamic from "next/dynamic";
+import CircularIndeterminate from "@/components/ui/CircularProgressIndicator";
 
 const App = () => {
   const user = useSelector(selectUser);
-
+  const [isLoading, setLoading] = useState(true);
 
   const dispatch = useDispatch();
   //this can be imporved
@@ -28,22 +26,23 @@ const App = () => {
     //set the user provider with user data
     onAuthStateChanged(auth, async (user) => {
       const userData = await generateUserDocument(user);
-      console.info(userData)
-      console.info(user)
-      console.info(`auth: ${auth.currentUser}`)
+      console.info(userData);
+      console.info(user);
+      console.info(`auth: ${auth.currentUser}`);
       const { uid, displayName, email, photoURL } = auth.currentUser;
       dispatch(setUser({ uid, displayName, email, photoURL }));
-
+      setLoading(false);
     });
   }, [dispatch]);
-//check login status and return component
-  return user ? (
-
+  //check login status and return component
+  return isLoading ? (
+    <div className=" h-screen flex items-center justify-center">
+      <CircularIndeterminate />
+    </div>
+  ) : user ? (
     <Welcome path="/" />
-
-
   ) : (
-    <div >
+    <div>
       <Header />
       <SignIn path="/" />
     </div>
@@ -51,5 +50,5 @@ const App = () => {
 };
 
 export default dynamic(() => Promise.resolve(App), {
-  ssr: false
+  ssr: false,
 });
